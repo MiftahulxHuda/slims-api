908ad1611911dfcf92f461ef4bc6e9a59487db6b
Miftahul Huda <huda@tura.group>
2021-03-15 19:30:04 +0700

    apply typeorm mysql & config database

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+import * as config from 'config';
+const serverConfig = config.get('server');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(new ValidationPipe());
+  
   const config = new DocumentBuilder()
     .setTitle('SLiMS Bulian API')
     .setDescription('The API focused in feature bibliography')
     .setVersion('1.0')
-    // .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const port = serverConfig.port;
+  await app.listen(port);
 }
 bootstrap();
